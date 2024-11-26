a5501c23ab1d3859ca090109688aa97854a80f91
Denis <dddenisss35@gmail.com>
2024-11-26 09:51:47 +0200

    Refactored

--- StateMachine.Pro/System.StateMachine/StateBase.cs
+++ StateMachine.Pro/System.StateMachine/StateBase.cs
@@ -12,12 +12,69 @@ public abstract class StateBase<TThis> where TThis : StateBase<TThis> {
     }
 
     // Owner
-    private IStateful<TThis>? Owner { get; set; } = null;
+    private protected IStateful<TThis>? Owner { get; private set; } = null;
     // Activity
-    public Activity_ Activity { get; private set; } = Activity_.Inactive;
+    public Activity_ Activity { get; private protected set; } = Activity_.Inactive;
 
     // Stateful
     public IStateful<TThis>? Stateful => Owner;
+
+    // OnAttach
+    public event Action<object?>? OnBeforeAttachEvent;
+    public event Action<object?>? OnAfterAttachEvent;
+    public event Action<object?>? OnBeforeDetachEvent;
+    public event Action<object?>? OnAfterDetachEvent;
+
+    // Constructor
+    private protected StateBase() {
+    }
+
+    // Attach
+    internal void Attach(IStateful<TThis> owner, object? argument) {
+        Assert.Operation.Message( $"State {this} must have no owner" ).Valid( Owner == null );
+        Assert.Operation.Message( $"State {this} must be inactive" ).Valid( Activity is Activity_.Inactive );
+        {
+            Owner = owner;
+            OnBeforeAttach( argument );
+            OnAttach( argument );
+            OnAfterAttach( argument );
+        }
+        Activate( argument );
+    }
+    internal void Detach(IStateful<TThis> owner, object? argument) {
+        Assert.Operation.Message( $"State {this} must have {owner} owner" ).Valid( Owner == owner );
+        Assert.Operation.Message( $"State {this} must be active" ).Valid( Activity is Activity_.Active );
+        Deactivate( argument );
+        {
+            OnBeforeDetach( argument );
+            OnDetach( argument );
+            OnAfterDetach( argument );
+            Owner = null;
+        }
+    }
+
+    // OnAttach
+    protected virtual void OnBeforeAttach(object? argument) {
+        OnBeforeAttachEvent?.Invoke( argument );
+    }
+    protected abstract void OnAttach(object? argument);
+    protected virtual void OnAfterAttach(object? argument) {
+        OnAfterAttachEvent?.Invoke( argument );
+    }
+    protected virtual void OnBeforeDetach(object? argument) {
+        OnBeforeDetachEvent?.Invoke( argument );
+    }
+    protected abstract void OnDetach(object? argument);
+    protected virtual void OnAfterDetach(object? argument) {
+        OnAfterDetachEvent?.Invoke( argument );
+    }
+
+    // Activate
+    private protected abstract void Activate(object? argument);
+    private protected abstract void Deactivate(object? argument);
+
+}
+public abstract class StateBase2<TThis> : StateBase<TThis> where TThis : StateBase2<TThis> {
 
     // OnActivate
     public event Action<object?>? OnBeforeActivateEvent;
@@ -26,81 +83,47 @@ public abstract class StateBase<TThis> where TThis : StateBase<TThis> {
     public event Action<object?>? OnAfterDeactivateEvent;
 
     // Constructor
-    public StateBase() {
+    public StateBase2() {
     }
-
-    // Attach
-    internal void Attach(IStateful<TThis> owner, object? argument) {
-        Assert.Operation.Message( $"State {this} must have no owner" ).Valid( Owner == null );
-        Assert.Operation.Message( $"State {this} must be inactive" ).Valid( Activity is Activity_.Inactive );
-        Owner = owner;
-        //OnAttach( argument );
-        Activate( argument );
-    }
-    internal void Detach(IStateful<TThis> owner, object? argument) {
-        Assert.Operation.Message( $"State {this} must have {owner} owner" ).Valid( Owner == owner );
-        Assert.Operation.Message( $"State {this} must be active" ).Valid( Activity is Activity_.Active );
-        Deactivate( argument );
-        //OnDetach( argument );
-        Owner = null;
-    }
-
-    //// OnAttach
-    //protected abstract void OnAttach(object? argument);
-    //protected abstract void OnDetach(object? argument);
 
     // Activate
-    private void Activate(object? argument) {
+    private protected sealed override void Activate(object? argument) {
         Assert.Operation.Message( $"State {this} must have owner" ).Valid( Owner != null );
         Assert.Operation.Message( $"State {this} must be inactive" ).Valid( Activity is Activity_.Inactive );
-        OnBeforeActivateInternal( argument );
+        OnBeforeActivate( argument );
         Activity = Activity_.Activating;
         {
             OnActivate( argument );
         }
         Activity = Activity_.Active;
-        OnAfterActivateInternal( argument );
+        OnAfterActivate( argument );
     }
-    private void Deactivate(object? argument) {
+    private protected sealed override void Deactivate(object? argument) {
         Assert.Operation.Message( $"State {this} must have owner" ).Valid( Owner != null );
         Assert.Operation.Message( $"State {this} must be active" ).Valid( Activity is Activity_.Active );
-        OnBeforeDeactivateInternal( argument );
+        OnBeforeDeactivate( argument );
         Activity = Activity_.Deactivating;
         {
             OnDeactivate( argument );
         }
         Activity = Activity_.Inactive;
-        OnAfterDeactivateInternal( argument );
-    }
-
-    // OnActivate
-    private void OnBeforeActivateInternal(object? argument) {
-        OnBeforeActivateEvent?.Invoke( argument );
-        OnBeforeActivate( argument );
-    }
-    private void OnAfterActivateInternal(object? argument) {
-        OnAfterActivate( argument );
-        OnAfterActivateEvent?.Invoke( argument );
-    }
-    private void OnBeforeDeactivateInternal(object? argument) {
-        OnBeforeDeactivateEvent?.Invoke( argument );
-        OnBeforeDeactivate( argument );
-    }
-    private void OnAfterDeactivateInternal(object? argument) {
         OnAfterDeactivate( argument );
-        OnAfterDeactivateEvent?.Invoke( argument );
     }
 
     // OnActivate
     protected virtual void OnBeforeActivate(object? argument) {
+        OnBeforeActivateEvent?.Invoke( argument );
     }
     protected abstract void OnActivate(object? argument);
     protected virtual void OnAfterActivate(object? argument) {
+        OnAfterActivateEvent?.Invoke( argument );
     }
     protected virtual void OnBeforeDeactivate(object? argument) {
+        OnBeforeDeactivateEvent?.Invoke( argument );
     }
     protected abstract void OnDeactivate(object? argument);
     protected virtual void OnAfterDeactivate(object? argument) {
+        OnAfterDeactivateEvent?.Invoke( argument );
     }
 
 }
